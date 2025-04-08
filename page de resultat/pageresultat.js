5a2efcb15f05f3cbbdc8d902cd5b139098c448e9
Dadv-a11y <darilvia64@gmail.com>
2025-04-08 22:24:34 +0100

    UPDATE

--- page de resultat/pageresultat.js
+++ page de resultat/pageresultat.js
@@ -9,35 +9,6 @@ const catalogue = fetch('../catalogue.json').then((response) => {
     return data;
 });
 
-async function fillMenu() {
-    let src;
-    if (displaymenu == 1) {
-        src = '../image/icon/fermer.png';
-        menuicon.src = src;
-        menu.style.display = 'block';
-        const data = await catalogue;
-        const ul = document.createElement('ul');
-        const categoriesAdded = new Set();
-        data.forEach((produit) => {
-            const categories = produit.categorie;
-            if (!categoriesAdded.has(categories)) {
-                categoriesAdded.add(categories);
-                const li = document.createElement('li');
-                const button = createButton(categories, '../page produit/pageproduit.html');
-                li.appendChild(button);
-                ul.appendChild(li);
-            }
-        });
-        menu.appendChild(ul);
-        displaymenu = 0;
-    } else {
-        src = '../image/icon/menu.png';
-        menuicon.src = src;
-        menu.style.display = 'none';
-        displaymenu = 1;
-        menu.innerHTML = null;
-    }
-}
 
 function createButton(text, url) {
     const button = document.createElement('button');
@@ -83,25 +54,50 @@ function createParagraph(key, value) {
     return p;
 }
 
-let panierData = JSON.parse(sessionStorage.getItem('panierdata')) || [];
-let quantite = sessionStorage.getItem('quantite') || 0;
-panier.textContent = quantite;
+
 let title = document.getElementById('title');
 let carteProduit = document.getElementById('carteProduit');
 let resultat = JSON.parse(sessionStorage.getItem('resultatRecherche'));
 
-async function fillpageResultat() {
-    const data = await catalogue;
+function fillpageResultat() {
     carteProduit.innerHTML = '';
     let p1 = document.createElement('p');
     p1.appendChild(document.createTextNode(`Nombre de résultats: ${resultat.length}`));
+    p1.style.position ='absolute';
+    p1.style.zIndex ='1'
+    p1.style.top ='8rem'
+    p1.style.right='45%'
+    carteProduit.style.marginTop='30px'
     carteProduit.appendChild(p1);
-
+    let id = 30 ;
     resultat.forEach((element) => {
-        const div3 = createProductCard(element);
-        carteProduit.appendChild(div3);
-    });
-}
+                const button = `   <button class="add_cart" 
+                data-id="${id}"
+                data-title="${element.nom}"
+                data-image="/${element.src}"
+                data-price="${element.prix}"
+                onclick="ajout(${id}, '${element.nom}', '/${element.src}', ${element.prix})"
+                >Ajouter au panier</buttoen>`;
+
+let card =`<div class="swiper-slide">  
+<div class="card">
+<div class="card_area">
+<img src="../${element.src}" alt="${element.nom}" class="card_image">
+</div>
+<div class="card_body">
+<h3 class="card_title">${element.nom}</h3>
+<p class="price">${element.prix}$</p>
+${button}
+</div>
+</div>
+</div>
+</div>`           
+ id++ ;  
+ carteProduit.insertAdjacentHTML('beforeend',card);
+                }
+       
+    )};
+
 
 function createProductCard(element) {
     const div1 = document.createElement('div');
@@ -154,35 +150,77 @@ async function ResultatRecherche() {
     }
 }
 
+function ajout(id , title , image , price){
+    const cartItem = {id, title, image ,price};
+    const cart = JSON.parse(localStorage.getItem('cart'))|| [];
+    cart.push(cartItem);
+    localStorage.setItem("cart",JSON.stringify(cart));
+    console.log('ajout');
+  };
+  
+
 document.addEventListener('DOMContentLoaded', fillpageResultat);
 
-function ajoutpanier(button, produit) {
-    let i = 0;
-    button.addEventListener('click', function () {
-        let exist = false;
-        let data = {};
-        data.nom = produit.nom;
-        data.prix = produit.prix;
-        data.src = produit.src ;
-        data.quantite = ++i;
-        if (panierData.length != 0) {
-            panierData.forEach((item) => {
-                if (item.nom == data.nom) {
-                    item.quantite++;
-                    exist = true;
-                }
-            });
-        }
-        if (!exist) {
-            panierData.push(data);
-        }
-        sessionStorage.setItem('panierdata', JSON.stringify(panierData));
-        quantite++;
-        panier.textContent = quantite;
-        sessionStorage.setItem('quantite', quantite);
-    });
+
+
+const sidebar = document.getElementById('sidebar');
+const mainContent = document.getElementById('main-content');
+const toggleButton = document.querySelector('.sidebar-toggle');
+let isdisplay = true
+toggleButton.addEventListener('click', () => {
+     if(!isdisplay){
+      mainContent.style.marginLeft ='0' ;
+      sidebar.style.visibility ='hidden';
+      console.log('bon:', isdisplay)
+      isdisplay= true ;
+     }
+     else{
+      mainContent.style.marginLeft = '250px' ;
+      sidebar.style.visibility ='visible';
+      console.log('mauvais:', isdisplay)
+      isdisplay=false;
+     }
+});
+let sidebarMenu = document.getElementById('sidebar-menu')
+async function fillMenu() {
+      const data = await catalogue;
+      const categoriesAdded = new Set();
+      let p = `<p>CATEGORIES</p>`;
+      sidebarMenu.insertAdjacentHTML('beforeend',p);
+      data.forEach((produit) => {
+          const categories = produit.categorie;
+          if (!categoriesAdded.has(categories)) {
+              categoriesAdded.add(categories);
+              const button = createButton(categories, '../page produit/pageproduit.html',categories);
+              sidebarMenu.appendChild(button);
+              sidebarMenu.appendChild(document.createElement('br'));
+          }
+
+      });
+}
+function createButton(text, href, produit) {
+  const button = document.createElement('button');
+  button.textContent = text;
+  
+  if (href) {
+      button.addEventListener('click', function () {
+          sessionStorage.setItem('pageproduit', produit);
+          window.location.href = href;
+      });
+  } else if (produit) {
+      ajoutpanier(button, produit);
+  }
+  return button;
 }
 
-function paniers() {
-    window.location.href = '../page du panier/panier.html';
-}
+let admin  = JSON.parse(sessionStorage.getItem('session')) || {};
+ if(admin.email== "admin" && admin.password ==  "admin" && admin.length != 0 ){
+  let DASHBOARD = document.getElementById('DASHBOARD'); 
+  DASHBOARD.style.display='inline';
+ }
+ else{
+    let DASHBOARD = document.getElementById('DASHBOARD');
+    DASHBOARD.style.display ='none';
+ }
+
+document.addEventListener('DOMContentLoaded',fillMenu)
