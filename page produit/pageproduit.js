827f6ec35d5f450efac464837ef5941b6d86fe6c
Dadv-a11y <darilvia64@gmail.com>
2025-04-08 22:25:37 +0100

    UPDATE

--- page produit/pageproduit.js
+++ page produit/pageproduit.js
@@ -10,47 +10,7 @@ const catalogue= fetch('../catalogue.json').then((response)=>{
     return data 
 })
 
-async function fillMenu() {
-    let src 
-     //pour afficher le menu
-    if(displaymenu ==1){
-        src='../image/icon/fermer.png'
-        menuicon.src = src
-         menu.style.display='block'
-        const data = await catalogue;
-        const ul = document.createElement('ul');
-        // Création d'un ensemble pour stocker les catégories déjà ajoutées
-        const categoriesAdded = new Set();
-          data.forEach((produit) => {
-              const categories = produit.categorie;
-              // Vérification si la catégorie n'a pas déjà été ajoutée
-              if (!categoriesAdded.has(categories)) {
-                categoriesAdded.add(categories);
-                const li = document.createElement('li');
-                const button = document.createElement('Button')
-                button.appendChild(document.createTextNode(categories));
-                  // pour generer la page lie au produit
-                  button.addEventListener('click',function(){
-                    let text = button.textContent
-                     sessionStorage.setItem('pageproduit', text)
-                     window.location.href='pageproduit.html'
-                })
-                li.appendChild(button);
-                ul.appendChild(li);
-              }
-            });
-         menu.appendChild(ul);
-         displaymenu=0
-     }
-     // pour cacher le menu
-     else{
-        src='../image/icon/menu.png'
-        menuicon.src = src
-        menu.style.display='none'
-        displaymenu=1
-        menu.innerHTML=null
-     }  
-}
+
 
 let displayuser = 1 ;
 let userInformation = document.getElementById('user-data')
@@ -82,10 +42,7 @@ function login() {
     }
    }
 
-   let panierData = JSON.parse(sessionStorage.getItem('panierdata')) || [];
-   let panier = document.getElementById('panier')
-   let quantite = sessionStorage.getItem('quantite')||0
-   panier.textContent= quantite
+  
    let title = document.getElementById('title')
    let carteProduit = document.getElementById('carteProduit')
    // pour recuperer la categorie selectionner dans le menu
@@ -94,48 +51,48 @@ function login() {
 
      // fonction pour crrer le contenu de page produit
     async function fillpageproduit(){
+        let id = 20 ;
         const data = await catalogue;
         data.forEach((produit)=>{
+            let p = document.createElement('p');
             if( produit.categorie == produitAafficher){
-            const div1= document.createElement('div')                   
-            div1.classList.add('imageproduit')
-            let img = document.createElement('img')
-            img.src ='../'+produit.src
-            img.alt = produit.nom
-            div1.appendChild(img)
-            const div2 = document.createElement('div')
-            div2.classList.add('descriptionproduit')
-            const div3 = document.createElement('div')
-            div3.classList.add('presentationduproduit')
-            let p = document.createElement('p')
-            p.appendChild(document.createTextNode(produit.nom))
-            p.appendChild(document.createElement('br'))
-            p.appendChild(document.createTextNode('couleur :'+produit.couleur))
-            let prix = produit.prix * 624 
-            p.appendChild(document.createElement('br'))
-            p.appendChild(document.createTextNode(prix+'fcfa'))
-            p.appendChild(document.createElement('br'))
-            let button = document.createElement('button')
-            button.classList.add('ajouteraupanier')
-            button.textContent='ajouter au panier'
-            ajoutpanier(button , produit)
-            const caracteristiques = produit.caracteristiques
-            for (const key in caracteristiques) {
-                p.appendChild(document.createTextNode(`${key}: ${caracteristiques[key]}`));
-                p.appendChild(document.createElement('br')); 
-            }
-            p.appendChild(button)
-            div2.appendChild(p)
-            div3.appendChild(div1)
-            div3.appendChild(div2)
-            carteProduit.appendChild(div3)
+                const caracteristiques = produit.caracteristiques
+                for (const key in caracteristiques) {
+                    p.appendChild(document.createTextNode(`${key}: ${caracteristiques[key]}`));
+                    p.appendChild(document.createElement('br')); 
                 }
-                // <button>ajouter au panier</button>
+                const button = `   <button class="add_cart" 
+                data-id="${id}"
+                data-title="${produit.nom}"
+                data-image="/${produit.src}"
+                data-price="${produit.prix}"
+                onclick="ajout(${id}, '${produit.nom}', '/${produit.src}', ${produit.prix})"
+                >Ajouter au panier</button>`;
+
+let card =`<div class="swiper-slide">  
+<div class="card">
+<div class="card_area">
+<img src="../${produit.src}" alt="${produit.nom}" class="card_image">
+</div>
+<div class="card_body">
+<h3 class="card_title">${produit.nom}</h3>
+<p class="price">${produit.prix}$</p>
+${p.outerHTML}
+${button}
+</div>
+</div>
+</div>
+</div>`           
+ id++ ;  
+ carteProduit.insertAdjacentHTML('beforeend',card);
+                }
         })
     }
+  
     document.addEventListener('DOMContentLoaded', fillpageproduit)
-
+    
     async function ResultatRecherche() {
+        let rechercher = document.getElementById('rechercher');
         const data = await catalogue;
         console.log(rechercher.value);
         const resultat = rechercher.value.toLowerCase(); // Convertir en minuscules pour une recherche insensible à la casse
@@ -153,34 +110,74 @@ function login() {
         console.log(filteredProducts); // Affiche les produits qui correspondent à la recherche
     }
     
-    function ajoutpanier(button , produit){
-        let i =0 
-        button.addEventListener('click', function() {    
-            let exist = false
-             let data = {} ;                  
-             data.nom = produit.nom;
-             data.prix = produit.prix;
-             data.src = produit.src ;
-             data.quantite = ++i; 
-           if(panierData.length !=0){
-              for (const key in panierData) {
-                 if(panierData[key].nom == data.nom){
-                    panierData[key].quantite++
-                    exist = true
-                 }
-              }
-           }
-            if(!exist){
-             panierData.push(data);
-             console.log(exist)
-            }
-             console.log(panierData);
-             sessionStorage.setItem('panierdata', JSON.stringify(panierData)); 
-         quantite++;
-         panier.textContent = quantite;
-         sessionStorage.setItem('quantite', quantite);
-     });
+function ajout(id , title , image , price){
+    const cartItem = {id, title, image ,price};
+    const cart = JSON.parse(localStorage.getItem('cart'))|| [];
+    cart.push(cartItem);
+    localStorage.setItem("cart",JSON.stringify(cart));
+    console.log('ajout');
+  };
+   
+  const sidebar = document.getElementById('sidebar');
+const mainContent = document.getElementById('main-content');
+const toggleButton = document.querySelector('.sidebar-toggle');
+let isdisplay = true ;
+toggleButton.addEventListener('click', () => {
+     if(!isdisplay){
+      mainContent.style.marginLeft ='0' ;
+      sidebar.style.visibility ='hidden';
+      console.log('bon:', isdisplay)
+      isdisplay= true ;
      }
-     function paniers(){
-        window.location.href='../page du panier/panier.html'
+     else{
+      mainContent.style.marginLeft = '250px' ;
+      sidebar.style.visibility ='visible';
+      console.log('mauvais:', isdisplay)
+      isdisplay=false;
      }
+});
+
+let sidebarMenu = document.getElementById('sidebar-menu')
+async function fillMenu() {
+      const data = await catalogue;
+      const categoriesAdded = new Set();
+      let p = `<p>CATEGORIES</p>`;
+      sidebarMenu.insertAdjacentHTML('beforeend',p);
+      data.forEach((produit) => {
+          const categories = produit.categorie;
+          if (!categoriesAdded.has(categories)) {
+              categoriesAdded.add(categories);
+              const button = createButton(categories, 'pageproduit.html',categories);
+              sidebarMenu.appendChild(button);
+              sidebarMenu.appendChild(document.createElement('br'));
+          }
+
+      });
+}
+function createButton(text, href, produit) {
+  const button = document.createElement('button');
+  button.textContent = text;
+  
+  if (href) {
+      button.addEventListener('click', function () {
+          sessionStorage.setItem('pageproduit', produit);
+          window.location.href = href;
+      });
+  } else if (produit) {
+      ajoutpanier(button, produit);
+  }
+  return button;
+}
+
+
+let admin  = JSON.parse(sessionStorage.getItem('session')) || {};
+ if(admin.email== "admin" && admin.password ==  "admin" && admin.length != 0 ){
+  let DASHBOARD = document.getElementById('DASHBOARD'); 
+  DASHBOARD.style.display='inline';
+ }
+ else{
+    let DASHBOARD = document.getElementById('DASHBOARD');
+    DASHBOARD.style.display ='none';
+ }
+
+document.addEventListener('DOMContentLoaded',fillMenu)
